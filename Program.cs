eb115a19e4ae8c5fc1046ed57bddc6d65f977dea
Finn Jordal <finn@jordal.dk>
2014-03-03 14:18:18 +0100

    Omstrukturering

--- Program.cs
+++ Program.cs
@@ -15,65 +15,13 @@ namespace Client
 			client.Timeout = new TimeSpan(0, 20, 0); // 20 minutter til store downloads
 			client.BaseAddress = new Uri("http://dawa.aws.dk/");
 
-			AutocompleteVejnavne(client, "q=rødkildevej");
-			AutocompleteAdresser(client, "q=rødkildevej");
 			GetAdresse(client, "0a3f50a0-73bf-32b8-e044-0003ba298018");
 			GetAdresser(client, "vejnavn=Rødkildevej&postnr=2400");
 			GetAdresserStreaming(client, "kommunekode=0101");
+			AutocompleteVejnavne(client, "q=rødkildevej");
+			AutocompleteAdresser(client, "q=rødkildevej");
 
 			Console.ReadLine();
-		}
-
-
-		static void AutocompleteVejnavne(HttpClient client, string query)
-		{
-			try
-			{
-				string url = "vejnavne/autocomplete" + (query.Length == 0 ? "" : "?") + query;
-				Console.WriteLine("GET " + url);
-				HttpResponseMessage response = client.GetAsync(url).Result;
-				response.EnsureSuccessStatusCode();
-				string responseBody = response.Content.ReadAsStringAsync().Result;
-				dynamic vejnavne = JArray.Parse(responseBody);
-
-				int antal = 0;
-				foreach (dynamic vejnavn in vejnavne)
-				{
-					Console.WriteLine(formatVejnavneAutocomplete(vejnavn));
-					antal++;
-				}
-				Console.WriteLine("{0} adresser", antal);
-			}
-			catch (HttpRequestException e)
-			{
-				Console.WriteLine("Message :{0} ", e.Message);
-			}
-		}
-
-
-		static void AutocompleteAdresser(HttpClient client, string query)
-		{
-			try
-			{
-				string url = "adresser/autocomplete" + (query.Length == 0 ? "" : "?") + query;
-				Console.WriteLine("GET " + url);
-				HttpResponseMessage response = client.GetAsync(url).Result;
-				response.EnsureSuccessStatusCode();
-				string responseBody = response.Content.ReadAsStringAsync().Result;
-				dynamic adresser = JArray.Parse(responseBody);
-
-				int antal = 0;
-				foreach (dynamic adresse in adresser)
-				{
-					Console.WriteLine(formatAdresseAutocomplete(adresse));
-					antal++;
-				}
-				Console.WriteLine("{0} adresser", antal);
-			}
-			catch (HttpRequestException e)
-			{
-				Console.WriteLine("Message :{0} ", e.Message);
-			}
 		}
 
 		static void GetAdresse(HttpClient client, string id)
@@ -151,7 +99,58 @@ namespace Client
 				Console.WriteLine("Message :{0} ", e.Message);
 			}
 		}
-		
+
+
+		static void AutocompleteVejnavne(HttpClient client, string query)
+		{
+			try
+			{
+				string url = "vejnavne/autocomplete" + (query.Length == 0 ? "" : "?") + query;
+				Console.WriteLine("GET " + url);
+				HttpResponseMessage response = client.GetAsync(url).Result;
+				response.EnsureSuccessStatusCode();
+				string responseBody = response.Content.ReadAsStringAsync().Result;
+				dynamic vejnavne = JArray.Parse(responseBody);
+
+				int antal = 0;
+				foreach (dynamic vejnavn in vejnavne)
+				{
+					Console.WriteLine(formatVejnavneAutocomplete(vejnavn));
+					antal++;
+				}
+				Console.WriteLine("{0} adresser", antal);
+			}
+			catch (HttpRequestException e)
+			{
+				Console.WriteLine("Message :{0} ", e.Message);
+			}
+		}
+
+		static void AutocompleteAdresser(HttpClient client, string query)
+		{
+			try
+			{
+				string url = "adresser/autocomplete" + (query.Length == 0 ? "" : "?") + query;
+				Console.WriteLine("GET " + url);
+				HttpResponseMessage response = client.GetAsync(url).Result;
+				response.EnsureSuccessStatusCode();
+				string responseBody = response.Content.ReadAsStringAsync().Result;
+				dynamic adresser = JArray.Parse(responseBody);
+
+				int antal = 0;
+				foreach (dynamic adresse in adresser)
+				{
+					Console.WriteLine(formatAdresseAutocomplete(adresse));
+					antal++;
+				}
+				Console.WriteLine("{0} adresser", antal);
+			}
+			catch (HttpRequestException e)
+			{
+				Console.WriteLine("Message :{0} ", e.Message);
+			}
+		}
+
 		static string formatAdresse(dynamic adresse)
 		{
 			return string.Format("{0} {1} {2} {3}, {4} {5}", adresse.adgangsadresse.vejstykke.navn, adresse.adgangsadresse.husnr, adresse.etage, adresse.dør, adresse.adgangsadresse.postnummer.nr, adresse.adgangsadresse.postnummer.navn);
